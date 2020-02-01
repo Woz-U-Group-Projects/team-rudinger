d70a4b8e550487ea546ecd69a444a0a0fcf8c21c
Charlie Rudinger <techsyslonghorn@gmail.com>
2020-02-01 13:03:41 -0600

    commit on master

--- client-react/src/components/Project.js
+++ client-react/src/components/Project.js
@@ -14,18 +14,18 @@ class Project extends React.Component {
 
   getData = () => {
     // Java Spring Boot uses port 8080
-    let url = "http://localhost:8080/tasks";
+    //let url = "http://localhost:8080/tasks";
 
     // C# dotnetcore uses port 5000
     //let url = "http://localhost:5000/projects";
 
     // Express uses port 3001 (react uses 3000)
-    //let url = "http://localhost:3001/tasks";
+    let url = "http://localhost:3001/tasks";
     axios.get(url).then(response => this.setState({ projects: response.data }));
   };
 
   addTask = () => {
-    let url = "http://localhost:8080/tasks";
+    let url = "http://localhost:3001/tasks";
     axios.post(url, { name: this.taskName.current.value }).then(response => {
       // refresh the data
       this.getData();
